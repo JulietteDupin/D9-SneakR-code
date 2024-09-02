c589c22727801feb4deb16edaca77df5c6a3f297
Betzalel <solene.lefeu@epitech.eu>
2024-09-02 14:19:34 +0200

    [ADD] | Front main page

--- Front/SneakR/src/App.jsx
+++ Front/SneakR/src/App.jsx
@@ -1,18 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-      <h1>test</h1>
-      </div>
-    </>
-  )
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </div>
+  );
 }
 
-export default App
+export default App;
