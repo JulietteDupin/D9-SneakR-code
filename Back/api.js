c85883c8a8de28c6bf93d3f2db9eab8d75ff627f
ael-tek <aelynn3.michenet@epitech.digital>
2024-09-02 11:41:52 +0200

    Added api to get sneakers + single sneaker

---
+++ Back/api.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+
+async function getSneakers() {
+    const filePath = 'sneakers.json';
+    const response = await fetch("http://54.37.12.181:1337/api/sneakers");
+    const sneakers = await response.json();
+    const jsonData = JSON.stringify(sneakers);
+
+    try {
+        fs.writeFileSync(filePath, jsonData);
+        console.log('Sneakers data saved to file successfully.');
+      } catch (error) {
+        console.error('Error writing sneakers data to file:', error);
+      }      
+  }
+  
+
+async function getSneaker(id) {
+    const response = await fetch(`http://54.37.12.181:1337/api/sneakers/${id}`);
+    const sneaker = await response.json();
+    
+    return sneaker;
+}   
