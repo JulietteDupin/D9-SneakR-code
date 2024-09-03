62aaf4aa665d60f5d76dae599471e5c24541ad9d
ael-tek <aelynn3.michenet@epitech.digital>
2024-09-03 15:04:10 +0200

    add hash

--- Back/routes/auth/login.js
+++ Back/routes/auth/login.js
@@ -5,9 +5,10 @@ const bcrypt = require('bcrypt');
 
 router.post('/login', async (req, res) => {
     const { email, password } = req.body;
-
+    const saltRounds = 10; 
+    const hashedPassword = await bcrypt.hash(password, saltRounds)
     try {
-        const result = db.query('SELECT * FROM users WHERE email = ? AND password = ?', [email, password], (err, results) => {  
+        const result = db.query('SELECT * FROM users WHERE email = ? AND password = ?', [email, hashedPassword], (err, results) => {  
             if (results.length > 0) {
                 //  const user = results[0];
                 localStorage.setItem('isAuthenticated', true);
