2e3e6eca9db3911efa5ac07e8decea575bacdde5
Betzalel <solene.lefeu@epitech.eu>
2024-09-03 16:35:42 +0200

    [ADD] | css for homepage

--- Front/SneakR/src/pages/Home.jsx
+++ Front/SneakR/src/pages/Home.jsx
@@ -1,9 +1,14 @@
-import { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useEffect, useState, } from 'react';
+import { useNavigate, useParams} from 'react-router-dom';
 import sneakersData from '../../../../Back/sneakers.json';
+import Navbar from '../tools/Navbar';
+import SneakerList from '../tools/SneakerList';
 
-export default function Home() {
+import '../../css/style.css';
+
+export default function Home({ setSelectedSneaker }) {
   const navigate = useNavigate();
+  const { gender } = useParams();
   const [sneakers, setSneakers] = useState(sneakersData.data);
 
   useEffect(() => {
@@ -14,19 +19,14 @@ export default function Home() {
   }, [navigate]);
 
   return (
-    <div>
-      <h1>Home Page</h1>
-      <ul>
-        {sneakers.map(sneaker => (
-          <li key={sneaker.id}>
-            {/* ici à terme au lieu de faire du hard code je ferai une fonction pour mapper */}
-            <img src={sneaker.attributes.image.small} alt={sneaker.name} />
-            <p>{sneaker.attributes.name}</p>
-            <p>Brand: {sneaker.attributes.brand}</p>
-            <p>Colorway: {sneaker.attributes.colorway}</p>
-          </li>
-        ))}
-      </ul>
+    <div className='frame'>
+      <Navbar />
+      <SneakerList
+        sneakers={sneakers}
+        gender={gender || "all"}
+        setSelectedSneaker={setSelectedSneaker}
+      />
     </div>
   );
 }
+
