2027efa019be24a94cb17411f4826124235174b9
root <root@DESKTOP-DG6J3C4>
2024-09-03 17:04:00 +0200

    [REBASE] dev

--- Front/SneakR/src/App.jsx
+++ Front/SneakR/src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
+import Payment from './pages/Payment';
 
 export default function App() {
   return (
@@ -10,6 +11,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/payment" element={<Payment />} />
         <Route path="/register" element={<Register />} />
       </Routes>
     </div>
