b75fffa0f3273e8b40fee89b916a5b29ef538040
root <root@DESKTOP-DG6J3C4>
2024-09-03 10:25:55 +0200

    [FIX] users routes

--- Back/routes/users.js
+++ Back/routes/users.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const db = require('../config/db');
 
 // Get all users
-router.get('/users', async (req, res) => {
+router.get('/', async (req, res) => {
   try {
     const [rows] = await db.query('SELECT * FROM users');
     res.json(rows);
@@ -13,7 +13,7 @@ router.get('/users', async (req, res) => {
 });
 
 // Get a user by ID
-router.get('/users/:id', async (req, res) => {
+router.get('/:id', async (req, res) => {
   const { id } = req.params;
   try {
     const [rows] = await db.query('SELECT * FROM users WHERE id = ?', [id]);
@@ -27,7 +27,7 @@ router.get('/users/:id', async (req, res) => {
 });
 
 // Create a new user
-router.post('/users', async (req, res) => {
+router.post('/', async (req, res) => {
   const { username, email, password } = req.body;
   try {
     const [result] = await db.query(
@@ -41,7 +41,7 @@ router.post('/users', async (req, res) => {
 });
 
 // Update a user by ID
-router.put('/users/:id', async (req, res) => {
+router.put('/:id', async (req, res) => {
   const { id } = req.params;
   const { username, email, password } = req.body;
   try {
@@ -59,7 +59,7 @@ router.put('/users/:id', async (req, res) => {
 });
 
 // Delete a user by ID
-router.delete('/users/:id', async (req, res) => {
+router.delete('/:id', async (req, res) => {
   const { id } = req.params;
   try {
     const [result] = await db.query('DELETE FROM users WHERE id = ?', [id]);
