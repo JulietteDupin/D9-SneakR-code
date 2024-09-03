2204bd8fb361adfd1a35eb5cf54d55de16a57ba1
ael-tek <aelynn3.michenet@epitech.digital>
2024-09-03 15:11:39 +0200

    update hash

--- Back/routes/auth/login.js
+++ Back/routes/auth/login.js
@@ -6,13 +6,18 @@ const bcrypt = require('bcrypt');
 router.post('/login', async (req, res) => {
     const { email, password } = req.body;
     const saltRounds = 10; 
-    const hashedPassword = await bcrypt.hash(password, saltRounds)
-    try {
+     try {
         const result = db.query('SELECT * FROM users WHERE email = ? AND password = ?', [email, hashedPassword], (err, results) => {  
             if (results.length > 0) {
+                bcrypt.compare(password, result.password, function(err, result) {
+                    console.log(result)
+                    if (result = true) {
+                        localStorage.setItem('isAuthenticated', true);
+                        alert('Login succeeded')
+                    }
+                });
                 //  const user = results[0];
-                localStorage.setItem('isAuthenticated', true);
-                alert('Login succeeded')
+                
             };
         })
     } catch(err) {
