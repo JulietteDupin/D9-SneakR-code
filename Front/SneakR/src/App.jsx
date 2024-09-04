eff73b4bed247738f4a64ae8825578cd42e5604d
Betzalel <solene.lefeu@epitech.eu>
2024-09-04 10:43:52 +0200

    [ADD] | pagination

--- Front/SneakR/src/App.jsx
+++ Front/SneakR/src/App.jsx
@@ -4,6 +4,7 @@ import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import ProductPage from './pages/ProductPage';
+import CategoryPage from './pages/CategoryPage';
 
 export default function App() {
   const [selectedSneaker, setSelectedSneaker] = useState(null);
@@ -13,10 +14,11 @@ export default function App() {
         <Routes>
           <Route path="/" element={<Home setSelectedSneaker={setSelectedSneaker} />} />
           <Route path="/product" element={<ProductPage sneaker={selectedSneaker} />} />
-          <Route path="/category/:gender" element={<Home setSelectedSneaker={setSelectedSneaker} />} />
+          <Route path="/category/:gender" element={<CategoryPage setSelectedSneaker={setSelectedSneaker} />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
         </Routes>
     </div>
   );
 }
+
---
+++ Front/SneakR/src/tools/Pagination.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import "../../css/pagination.css"
+
+const Pagination = ({ sneakersPerPage, totalSneakers, paginate, currentPage }) => {
+    const pageNumbers = [];
+
+    for (let i = 1; i <= Math.ceil(totalSneakers / sneakersPerPage); i++) {
+        pageNumbers.push(i);
+    }
+
+    return (
+        <nav>
+            <ul className="pagination">
+                {pageNumbers.map(number => (
+                    <li key={number} className={`page-item ${currentPage === number ? 'active' : ''}`}>
+                        <button onClick={() => paginate(number)} className="page-link">
+                            {number}
+                        </button>
+                    </li>
+                ))}
+            </ul>
+        </nav>
+    );
+};
+
+export default Pagination;
