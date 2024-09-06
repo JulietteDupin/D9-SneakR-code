e506532ac5cd2a681f0fc958e9d3cab74dcd996a
Betzalel <solene.lefeu@epitech.eu>
2024-09-06 16:22:13 +0200

    FIX | Shoes retrieval in navbar and navigation

--- Front/SneakR/src/pages/CategoryPage.jsx
+++ Front/SneakR/src/pages/CategoryPage.jsx
@@ -1,31 +1,40 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
-import sneakersData from '../../../../Back/sneakers.json';
 import Navbar from '../tools/Navbar';
 import SneakerList from '../tools/SneakerList';
-import Pagination from '../tools/Pagination';
+import {
+  Pagination,
+  PaginationContent,
+  PaginationEllipsis,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from "@/components/ui/pagination";
 
 import '../../css/style.css';
 
 export default function CategoryPage({ setSelectedSneaker }) {
   const navigate = useNavigate();
   const { gender } = useParams();
-  const [sneakers, setSneakers] = useState(sneakersData.data);
-
+  const [sneakers, setSneakers] = useState([]);
+  const [filteredSneakers, setFilteredSneakers] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
-  const sneakersPerPage = 25;
+  const sneakersPerPage = 100;
+  const fetchDataRef = useRef();
+
   const indexOfLastSneaker = currentPage * sneakersPerPage;
   const indexOfFirstSneaker = indexOfLastSneaker - sneakersPerPage;
-
-  const filteredSneakers = !gender
-    ? sneakers
-    : sneakers.filter(sneaker => sneaker.gender.toLowerCase() === gender.toLowerCase());
-
-
   const currentSneakers = filteredSneakers.slice(indexOfFirstSneaker, indexOfLastSneaker);
+
+  const totalPages = Math.ceil(filteredSneakers.length / sneakersPerPage);
   const paginate = (pageNumber) => setCurrentPage(pageNumber);
 
-  // TODO: changer pour un token JWT
+  const maxVisiblePages = 3;
+
+  const startPage = Math.max(currentPage - 1, 1);
+  const endPage = Math.min(startPage + maxVisiblePages - 1, totalPages);
+
   useEffect(() => {
     const token = localStorage.getItem('token');
 
@@ -33,6 +42,33 @@ export default function CategoryPage({ setSelectedSneaker }) {
       navigate('/login');
     }
   }, [navigate]);
+
+  useEffect(() => {
+    fetchDataRef.current = async () => {
+      try {
+        const response = await fetch(import.meta.env.VITE_APP_PRODUCTS_ROUTE, {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "*",
+          },
+        });
+        const data = await response.json();
+        setSneakers(data);
+        setFilteredSneakers(
+          !gender
+            ? data
+            : data.filter((sneaker) => sneaker.gender.toLowerCase() === gender.toLowerCase())
+        );
+      } catch (error) {
+        console.error("Error:", error);
+        setError(error.message);
+      }
+    };
+
+    fetchDataRef.current();
+  }, [gender]);
+
 
   return (
     <div className='frame'>
@@ -43,12 +79,51 @@ export default function CategoryPage({ setSelectedSneaker }) {
         setSelectedSneaker={setSelectedSneaker}
       />
 
-      <Pagination
-        sneakersPerPage={sneakersPerPage}
-        totalSneakers={currentSneakers.length}
-        paginate={paginate}
-        currentPage={currentPage}
-      />
+      <Pagination>
+        <PaginationContent>
+          {currentPage > 1 && (
+            <PaginationItem>
+              <PaginationPrevious
+                onClick={() => paginate(currentPage - 1)}
+                aria-label="Previous"
+              />
+            </PaginationItem>
+          )}
+
+          {startPage > 1 && (
+            <PaginationItem>
+              <PaginationEllipsis />
+            </PaginationItem>
+          )}
+
+          {Array.from({ length: endPage - startPage + 1 }, (_, i) => startPage + i).map((page) => (
+            <PaginationItem key={page}>
+              <PaginationLink
+                href="#"
+                onClick={() => paginate(page)}
+                isActive={page === currentPage}
+              >
+                {page}
+              </PaginationLink>
+            </PaginationItem>
+          ))}
+
+          {endPage < totalPages && (
+            <PaginationItem>
+              <PaginationEllipsis />
+            </PaginationItem>
+          )}
+
+          {currentPage < totalPages && (
+            <PaginationItem>
+              <PaginationNext
+                onClick={() => paginate(currentPage + 1)}
+                aria-label="Next"
+              />
+            </PaginationItem>
+          )}
+        </PaginationContent>
+      </Pagination>
 
     </div>
   );
