d2b58417f43d351fc9ab49dac97d36dbc5b65527
Betzalel <solene.lefeu@epitech.eu>
2024-09-10 14:36:23 +0200

    [FIX] | possible choices for user preferences brands

--- Back/routes/products.js
+++ Back/routes/products.js
@@ -2,13 +2,27 @@ const express = require('express');
 const router = express.Router();
 const db = require('../config/db');
 
+// Get all sneakers
 router.get('/', async (req, res) => {
-    try {
-      const [sneakers] = await db.query('SELECT * FROM sneakers');
-      res.json(sneakers)
-    } catch (err) {
-      console.log(err)
-    }
+  try {
+    const [sneakers] = await db.query('SELECT * FROM sneakers');
+    res.json(sneakers)
+  } catch (err) {
+    console.error('Erreur lors de la récupération des marques:', err);
+    res.status(500).json({ message: 'Server error', error: err.message });
+
+  }
+});
+
+// Get all sneaker brands
+router.get('/brands', async (req, res) => {
+  try {
+    const [brands] = await db.query('SELECT DISTINCT brand FROM sneakers');
+    res.json(brands);
+  } catch (err) {
+    console.error('Erreur lors de la récupération des marques:', err);
+    res.status(500).json({ message: 'Server error', error: err.message });
+  }
 });
 
 module.exports = router;
