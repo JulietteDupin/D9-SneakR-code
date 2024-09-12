5907d429490e59100dde90763512c10d571765c1
Betzalel <solene.lefeu@epitech.eu>
2024-09-12 12:21:41 +0200

    FIX | brand choice in user account

--- Back/routes/products.js
+++ Back/routes/products.js
@@ -2,6 +2,17 @@ const express = require('express');
 const router = express.Router();
 const db = require('../config/db');
 
+// Get all sneaker brands
+router.get('/brands', async (req, res) => {
+  try {
+    const [brands] = await db.query('SELECT DISTINCT brand FROM sneakers');
+    res.json(brands);
+  } catch (err) {
+    console.error('Erreur lors de la récupération des marques:', err);
+    res.status(500).json({ message: 'Server error', error: err.message });
+  }
+});
+
 router.get('/:id', async (req, res) => {
   const { id } = req.params;
   try {
@@ -23,17 +34,6 @@ router.get('/', async (req, res) => {
     console.error('Erreur lors de la récupération des marques:', err);
     res.status(500).json({ message: 'Server error', error: err.message });
 
-  }
-});
-
-// Get all sneaker brands
-router.get('/brands', async (req, res) => {
-  try {
-    const [brands] = await db.query('SELECT DISTINCT brand FROM sneakers');
-    res.json(brands);
-  } catch (err) {
-    console.error('Erreur lors de la récupération des marques:', err);
-    res.status(500).json({ message: 'Server error', error: err.message });
   }
 });
 
